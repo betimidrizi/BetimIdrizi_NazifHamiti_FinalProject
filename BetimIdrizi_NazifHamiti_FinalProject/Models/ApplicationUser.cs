3aad45420f788b9af094cc7ae75f8c5a03439337
betimidriz <betimidrizi55@gmail.com>
2025-02-05 21:30:03 +0100

    Added ApplicationUser Model

---
+++ BetimIdrizi_NazifHamiti_FinalProject/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BetimIdrizi_NazifHamiti_FinalProject.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Add custom properties if needed
+    }
+}
