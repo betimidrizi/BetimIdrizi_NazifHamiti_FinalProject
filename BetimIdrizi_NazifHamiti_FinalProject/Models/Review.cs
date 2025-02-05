e6e22ce05b2042b72fc0494941538cb1b6856034
Nazif Hamiti <nazif11xiki@gmail.com>
2025-02-05 11:58:51 -0800

    Review Model

---
+++ BetimIdrizi_NazifHamiti_FinalProject/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace BetimIdrizi_NazifHamiti_FinalProject.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }  // To track which user left the review
+        public string Comment { get; set; }  // The review text
+
+        public int BookId { get; set; }  // Link to the book
+    }
+}
