66ee61cdd303517e6c808dae293cc08d23c9d0f5
betimidriz <betimidrizi55@gmail.com>
2025-02-05 20:58:03 +0100

    Book Model

---
+++ BetimIdrizi_NazifHamiti_FinalProject/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BetimIdrizi_NazifHamiti_FinalProject.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+    }
+}
