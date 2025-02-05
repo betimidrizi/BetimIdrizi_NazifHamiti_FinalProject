42980e679e28e5db8a0497964e4df8fd8a01e8d5
Nazif Hamiti <nazif11xiki@gmail.com>
2025-02-05 12:55:38 -0800

    Added DbContext

---
+++ BetimIdrizi_NazifHamiti_FinalProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BetimIdrizi_NazifHamiti_FinalProject.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BetimIdrizi_NazifHamiti_FinalProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ BetimIdrizi_NazifHamiti_FinalProject/Data/IdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BetimIdrizi_NazifHamiti_FinalProject.Data
+{
+    public class IdentityDbContext<T>
+    {
+        private DbContextOptions<ApplicationDbContext> options;
+
+        public IdentityDbContext(DbContextOptions<ApplicationDbContext> options)
+        {
+            this.options = options;
+        }
+    }
+}
